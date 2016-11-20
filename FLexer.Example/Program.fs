b24bdbf1f04c6971fb7f23edd469495effef6366
daniel_oliver <danieleugeneoliver@gmail.com>
2016-11-20 03:14:28 -0600

    Incomplete rules example

--- FLexer.Example/Program.fs
+++ FLexer.Example/Program.fs
@@ -1,4 +1,84 @@
-﻿[<EntryPoint>]
+﻿open FLexer.Lexical.Tokenizer
+
+
+type [<RequireQualifiedAccess>] Keyword =
+  | Let
+  | False
+  | True
+
+type [<RequireQualifiedAccess>] Operator =
+  | EqualTo
+  | GreaterThan
+  | LessThan
+  | GreaterThanOrEqualTo
+  | LessThanOrEqualTo
+  | Multiply
+  | Divide
+
+type [<RequireQualifiedAccess>] Punctuation =
+  | OpenCurlyBracket
+  | CloseCurlyBracket
+  | OpenBracket
+  | CloseBracket
+  | OpenParentheses
+  | CloseParentheses
+  | SemiColon
+
+type [<RequireQualifiedAccess>] TokenType =
+  | Whitespace
+  | Keyword of Keyword
+  | Integer of int
+  | Decimal of decimal
+  | Identifier of string
+  | StringLiteral of string
+  | NewLine
+  | Operator of Operator
+  | CustomOperator of string
+  | Punctuation of Punctuation
+
+let rules =
+  [ Rule.Literal("let", fun _ -> TokenType.Keyword(Keyword.Let))
+    Rule.Literal("true", fun _ -> TokenType.Keyword(Keyword.True))
+    Rule.Literal("false", fun _ -> TokenType.Keyword(Keyword.False))
+
+    Rule.Regex(@"[0-9]+[.][0-9]+", System.Decimal.Parse >> TokenType.Decimal)
+    Rule.Regex(@"[0-9]+", System.Int32.Parse >> TokenType.Integer)
+    Rule.Regex(@"[A-Za-z][0-9A-Za-z]*", TokenType.Identifier)
+    Rule.Regex(@"[ ]+", fun _ -> TokenType.Whitespace)
+    Rule.Regex(@"(\r\n|\n|\r)", fun _ -> TokenType.NewLine)
+    
+    Rule.Literal("[", fun _ -> TokenType.Punctuation Punctuation.OpenBracket)
+    Rule.Literal("]", fun _ -> TokenType.Punctuation Punctuation.CloseBracket)
+    Rule.Literal("{", fun _ -> TokenType.Punctuation Punctuation.OpenCurlyBracket)
+    Rule.Literal("}", fun _ -> TokenType.Punctuation Punctuation.CloseCurlyBracket)
+    Rule.Literal("(", fun _ -> TokenType.Punctuation Punctuation.OpenParentheses)
+    Rule.Literal(")", fun _ -> TokenType.Punctuation Punctuation.CloseParentheses)
+    Rule.Literal(";", fun _ -> TokenType.Punctuation Punctuation.SemiColon)
+
+    Rule.Literal("=", fun _ -> TokenType.Operator Operator.EqualTo)
+    Rule.Literal(">", fun _ -> TokenType.Operator Operator.GreaterThan)
+    Rule.Literal("<", fun _ -> TokenType.Operator Operator.LessThan)
+    Rule.Literal(">=", fun _ -> TokenType.Operator Operator.GreaterThanOrEqualTo)
+    Rule.Literal("<=", fun _ -> TokenType.Operator Operator.LessThanOrEqualTo)
+    Rule.Literal("/", fun _ -> TokenType.Operator Operator.Divide)
+    Rule.Literal("*", fun _ -> TokenType.Operator Operator.Multiply)
+    
+    Rule.Regex(@"[!@#$%^&*<>?:~\-=_\/]+", TokenType.CustomOperator)
+    Rule.Regex("[\"]([^\"]|[\\\\][\"])*[\"]", TokenType.StringLiteral)
+  ]
+
+
+
+let example = "
+let Four = 5.0;
+"
+
+[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+  let tokens = Tokenize rules example
+
+  for i in tokens do
+    printfn "%A" i
+
+  System.Console.Read() |> ignore
+  0 // return an integer exit code
---
+++ FLexer.Lexical/Tokenizer.fs
@@ -0,0 +1,94 @@
+﻿module FLexer.Lexical.Tokenizer
+
+type Token<'t> =
+  { Text: string
+    Offset: int
+    TokenType: 't
+  }
+
+type [<RequireQualifiedAccess>] Rule<'t> =
+  | Regex of Pattern: string * Mapper: (string -> 't)
+  | Literal of Literal: string * Mapper: (string -> 't)
+
+
+type [<RequireQualifiedAccess>] EngineMatch<'t> =
+  | Success of Token<'t>
+  | Failure of string
+
+
+type Engine<'t>(rules: Rule<'t> list ) =
+  let _rules = 
+    rules 
+    |> Seq.mapi(fun index t -> index, t) 
+    |> Map.ofSeq
+  let _regex = 
+    _rules 
+    |> Seq.choose(fun t -> match t.Value with | Rule.Regex (pattern, mapping) -> Some(t.Key, pattern, mapping) | _ -> None)
+    |> Seq.map(fun (index, pattern, mapping) -> index, (new System.Text.RegularExpressions.Regex("\G" + pattern, System.Text.RegularExpressions.RegexOptions.Compiled)))
+    |> Map.ofSeq
+
+  let _occurrences (getFromString: string) (checkedChar: char) (maxChar: int) =
+    let rec loop i count = 
+      if i < getFromString.Length || i < maxChar then
+        if getFromString.[i] = checkedChar then loop (i+1) (count+1)
+        else loop (i+1) count
+      else count
+    loop 0 0
+
+//  let _occurrences (x: string, c: char) x |> Seq.filter()
+  let _line (x: string) (startAt: int) =  _occurrences x '\n' startAt
+  let _column (x: string) (startAt: int) =
+    match x.LastIndexOf('\n', startAt) with
+    | -1 -> startAt
+    | y -> startAt - y
+
+  let _getToken (startAt: int) (text: string) =
+    _rules
+    |> Seq.choose(fun t ->
+      let rule = t.Value
+      let position = t.Key
+      let translate (creator: string -> 't) (sample: string) = 
+        try creator sample 
+        with | _ as ex -> failwithf "Failed to translate \"%s\" with \"%s\"" sample ex.Message
+      match rule with 
+      | Rule.Regex(pattern, mapper) ->
+        let ruleRegex = _regex.[position]
+        match ruleRegex.Match(text, startAt) with
+        | null -> None
+        | x when x.Success -> 
+          Some( 
+            { Text = x.Value
+              TokenType = translate mapper x.Value
+              Offset = startAt
+            }, position)
+        | _ -> None
+      | Rule.Literal(literal, mapper) ->
+        if startAt + literal.Length > text.Length then None
+        else
+          let substr = text.Substring(startAt, literal.Length)
+          match substr.Equals(literal, System.StringComparison.InvariantCultureIgnoreCase) with
+          | false -> None
+          | true ->
+            Some(
+              { Text = substr
+                TokenType = translate mapper substr
+                Offset = startAt                                          
+               }, position)
+    ) 
+    |> Seq.sortByDescending(fun (t, position) -> t.Text.Length, System.Int32.MaxValue - position)
+    |> Seq.tryHead
+    |> (function | Some(x, _) -> x | None -> failwithf "Failed to find rule for position %i." startAt)
+
+  member this.TokenStream (text: string) =
+    let rec nextToken startAt =
+      let token = _getToken startAt text
+      let nextOffset = token.Text.Length + token.Offset
+      if nextOffset = text.Length then [ token ]
+      else token :: (nextToken nextOffset)  
+    nextToken 0
+    
+let Tokenize<'t>(rules: Rule<'t> list) (text: string) =
+  let engine = Engine rules
+  engine.TokenStream text
+
+
